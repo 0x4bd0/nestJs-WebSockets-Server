5df7e4b70ac62019c4fa4963236787750938ab43
0x4bd0 <abdoumjr@gmail.com>
2021-02-13 19:40:40 +0100

    setup alert controller

--- src/alert/alert.controller.ts
+++ src/alert/alert.controller.ts
@@ -1,10 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { AlertGateway } from 'src/alert.gateway';
 
 @Controller('alert')
 export class AlertController {
 
+    constructor(private alertGateWay : AlertGateway){}
+
     @Post('')
-    sendAllert(@Body : dto {msg:string}) : any {
-        
+    sendAllert(@Body()  dto : {msg:string}) : string {
+
+        this.alertGateWay.sendToAll(dto.msg)
+
+        return 'Message send successfully'
+    
     }
 }
