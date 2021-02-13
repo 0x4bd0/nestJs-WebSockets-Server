1cd164f37968f405a956419eefe6afd7686a87b4
0x4bd0 <abdoumjr@gmail.com>
2021-02-13 17:51:44 +0100

    update socket params

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -3,7 +3,7 @@ import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessa
 import { Server } from 'http';
 import { Socket } from 'socket.io'
 
-@WebSocketGateway(4959,{path:'/ws'})
+@WebSocketGateway(4959,{path:'/ws', serveClient : true, namespace :'/'})
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
   // this is used to send message to everyone 
