8999898dee9e7cfb6d0d50a8c0349a448fdf2090
0x4bd0 <abdoumjr@gmail.com>
2021-02-13 16:47:40 +0100

    gateway setup :fire:

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,9 +1,38 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
+import { Server } from 'http';
+import { Socket } from 'socket.io'
 
 @WebSocketGateway()
-export class AppGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  
+  // this is used to send message to everyone 
+  /* @WebSocketServer() wss : Server; */
+
+  private logger: Logger = new Logger('AppGatewayLogger')
+
+  handleDisconnect(client: any) {
+    this.logger.log('socket gate way disconnected')
+
   }
+  handleConnection(client: any, ...args: any[]) {
+    this.logger.log('socket gate way connected')
+
+  }
+
+  afterInit(server: any) {
+    this.logger.log('Web socker gateway init ...')
+  }
+
+  @SubscribeMessage('sendToServer')
+  handleMessage(client: Socket, text: string): WsResponse<string> {
+    // this is used to send message to everyone 
+    /* this.wss.emit('sendToServer',text) */
+
+    // THIS 
+ /*   client.emit('sendToServer',text)  */
+    // Equals this
+    return { event : 'sendToServer', data : 'Hello there :)' }
+  }
+
 }
